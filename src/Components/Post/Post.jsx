af53fbbe1cb86d448b5eebfa5146d857945015cb
Jim <merajuljim@gmai.com>
2025-10-02 17:43:27 +0600

    use navigate hook used

--- src/Components/Post/Post.jsx
+++ src/Components/Post/Post.jsx
@@ -1,15 +1,20 @@
-import { Link } from "react-router";
+import { Link, useNavigate } from "react-router";
 
 const Post = ({ postData }) => {
   const { id, title, userId } = postData;
+  const navigate = useNavigate();
+  const handleNavigate = () =>{
+    navigate(`/posts/${id}`);
+  }
   return (
-    <div className="border border-green-500 rounded-lg p-4 text-center mb-5">
+    <div className="border border-green-500 rounded-lg p-4 text-center mb-5 flex flex-col">
       <p className="text-2xl font-bold">UserId: {userId}</p>
       <p className="text-xl font-bold">PostId: {id}</p>
       <p className="text-xl font-semibold">Title: {title}</p>
       <button className="bg-green-800 rounded-full text-white font-bold p-2">
         <Link to={`/posts/${id}`}>See More</Link>
       </button>
+      <button onClick={handleNavigate}>See details of id: {id}</button>
     </div>
   );
 };
--- src/Components/PostDetails/PostDetails.jsx
+++ src/Components/PostDetails/PostDetails.jsx
@@ -1,15 +1,16 @@
-import { useLoaderData } from "react-router";
+import { useLoaderData, useNavigate } from "react-router";
 
 const PostDetails = () => {
     const postDetails = useLoaderData();
     const {id, userId, title, body} = postDetails;
+    const navigate = useNavigate();
     return (
         <div className="border border-blue-700 rounded-2xl text-center p-4">
             <h2 className="text-2xl text-blue-900">UserId: {userId}</h2>
             <p className="text-xl text-blue-900">PostId: {id}</p>
             <p className="text-xl text-blue-900 underline">{title}</p>
             <p className="text-blue-900">{body}</p>
-
+            <button onClick={()=>navigate(-1)} className="border p-1 border-red-500 rounded-2xl">Go Back</button>
         </div>
     );
 };
